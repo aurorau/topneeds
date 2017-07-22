aa1684a49448377aed51bc0947eea44bf4950ef3
aurorau <aurora.bcg@gmail.com>
2017-07-22 22:38:02 +0530

    initial project commit

---
+++ src/main/java/com/topneeds/TopneedsApplication.java
@@ -0,0 +1,12 @@
+package com.topneeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopneedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopneedsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/topneeds/TopneedsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.topneeds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TopneedsApplication.class)
+public class TopneedsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
